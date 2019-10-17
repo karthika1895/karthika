002ed75e80a2d49ef942c57af37e9d1a20d4a334
karthika1895 <karthikk2218@gmail.com>
2019-10-17 08:29:21 +0530

    second commit

---
+++ src/org/mouse/Sample11.java
@@ -0,0 +1,13 @@
+package org.mouse;
+
+public class Sample11 {
+	private void Model() {
+		// TODO Auto-generated method stub
+
+	}
+	private void Phone() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
